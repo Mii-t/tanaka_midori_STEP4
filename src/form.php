84523a73189352190cbe4f00537331fbfaa28a55
Mii-t <188024781+Mii-t@users.noreply.github.com>
2026-02-23 23:27:54 +0900

    first commit

--- src/form.php
+++ src/form.php
@@ -10,36 +10,54 @@
 <div class="container">
     <div class="card">
 
-      <h1>入力フォーム</h1>
+        <h1>入力フォーム</h1>
 
-      <from action="conform.php" method="post">
-        <label for="name">名前</label>
-        <input type="text" id="name" name="name"><br><br>
+        <form action="conform.php" method="post">
 
-        <label for="age">年齢</label>
-        <input type="text" id="age" name="age" min="0" max="150"><br><br>
+            <div class="form-group">
+                <label for="name">名前:</label>
+                <input type="text" id="name" name="name">
+            </div>
 
-        <label for="phone">電話番号</label>
-        <input type="text" id="phone" name="phone"><br><br>
+            <div class="form-group">
+                <label for="age">年齢:</label>
+                <input type="text" id="age" name="age" min="0" max="150">
+            </div>
 
-        <label for="email">メールアドレス</label>
-        <input type="text" id="email" name="email"><br><br>
+            <div class="form-group">
+                <label for="phone">電話番号:</label>
+                <input type="text" id="phone" name="phone">
+            </div>
 
-        <label for="address">住所</label>
-        <input type="text" id="address" name="address"><br><br>
+            <div class="form-group">
+                <label for="email">メールアドレス:</label>
+                <input type="text" id="email" name="email">
+            </div>
 
-        <label for="question">質問</label>
-        <input type="text" id="question" name="question"><br><br>
+            <div class="form-group">
+                <label for="address">住所:</label>
+                <input type="text" id="address" name="address">
+            </div>
 
-        <label for="gender">性別</label>
-        <select id="gender" name="gender">
-          <option value="">選択してください</option>
-          <option value="男性">男性</option>
-          <option value="女性">女性</option>
-        </select><br><br>
+            <div class="form-group">
+                <label for="question">質問:</label>
+                <input type="text" id="question" name="question">
+            </div>
 
-      <button type="submit">送信</button>
-      </from>
+            <div class="form-group">
+                <label for="gender">性別:</label>
+                <select id="gender" name="gender">
+                    <option value="">選択してください</option>
+                    <option value="男性">男性</option>
+                    <option value="女性">女性</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <button type="submit">送信</button>
+            </div>
+
+        </form>
 
     </div>
 </div>
